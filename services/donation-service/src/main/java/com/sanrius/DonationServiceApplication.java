17229dd3f7f40f95fdfbcb2720034c3a72cc46a8
magichustler <magichustler01@gmail.com>
2025-04-07 13:29:19 -0400

    Add donation-service

---
+++ services/donation-service/src/main/java/com/sanrius/DonationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sanrius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationServiceApplication.class, args);
+	}
+
+}
